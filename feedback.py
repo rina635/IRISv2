2cb9481bb94259dfc4569b7bf206f398566b0eb4
Rina <rina635@gmail.com>
2021-04-16 20:52:47 -0400

    started qa dictionary

--- feedback.py
+++ feedback.py
@@ -8,7 +8,11 @@ Created on Tue Apr 13 15:45:28 2021
 import os, sys, re
 import spacy
 from nltk import pos_tag
-  
+import pandas as pd
+import csv
+from gensim.summarization.summarizer import summarize
+from gensim.summarization import keywordss
+
 nlp = spacy.load('en_core_web_sm')
 
 #Finds location of log file
@@ -24,6 +28,7 @@ for filename in os.listdir(abspath):
 with open('user_response.log') as file:
     lines = file.read().splitlines() #creates a list of each line in log file
 
+
 #Printing these just to see what I'm dealing with here.
 len(lines)
 lines[0]
@@ -31,6 +36,49 @@ lines[1]
 lines[2]
 lines[3]
 lines[4]
+
+'''
+grab nounn chunks from each question into a list
+search response list for substrings that contains any
+add list of scores to dictionary'''
+
+#user response stores starting at index 3 and 4 lines after that
+#Creating a searate list of the user's answers for analyses.
+user_responses = []
+for i in range(3, len(lines),4):
+    response = (lines[i])
+    clean_response = response.split(': ')
+    resp_only = clean_response[1]
+    user_responses.append(resp_only)
+
+#Creates list of the questions IRIS asked but removes the chatbot's name
+questions = []
+for i in range(1, len(lines),4):
+    q = lines[i]
+    clean_q = q.split(': ')
+    q_only = clean_q[2]
+    questions.append(q_only)
+    
+    
+#Create dictionary of question and answers:
+ #https://careerkarma.com/blog/python-convert-list-to-dictionary/
+qa_dictionary = dict(zip(questions, user_responses))    
+#https://realpython.com/iterate-through-dictionary-python/#iterating-through-keys-directly
+#prints the noun chunks for each of the questions - want to search the respective answers to see if they contain them to dictate if appropriate context
+for key in qa_dictionary:
+    doc = nlp(key)
+    for np in doc.noun_chunks:
+        print(np.text)
+#https://stackoverflow.com/questions/12376079/check-if-any-value-of-a-dictionary-matches-a-condition
+#https://www.programiz.com/python-programming/methods/built-in/any
+
+#Returns true or false if string is in answer but has to be the entire string
+#doesn't do substring bc one of answers only had this will print True
+any(v == 'blah ba' for v in qa_dictionary.values())
+    
+
+#Create a list of keyword criteria from dictionary?
+#Evaluate the list of user responses based on that
 
 
 #Locates the Leadership Category Index
