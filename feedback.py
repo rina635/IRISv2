53b1cf992f83d21ed741ad6f92a308f53c8bae34
Rina <rina635@gmail.com>
2021-04-21 14:58:37 -0400

    cleaned up comments

--- feedback.py
+++ feedback.py
@@ -5,13 +5,10 @@ Created on Tue Apr 13 15:45:28 2021
 
 @author: Rina
 """
-import os, sys, re
-import spacy
+#importing libraries
+import os, sys, re, csv, spacy
 from nltk import pos_tag
 import pandas as pd
-import csv
-#from gensim.summarization.summarizer import summarize
-#from gensim.summarization import keywords
 from nltk import sent_tokenize
 import numpy as np
 from datetime import datetime
@@ -19,19 +16,18 @@ nlp = spacy.load('en_core_web_sm')
 
 #Finds location of log file
 #https://stackoverflow.com/questions/33832184/open-a-log-extension-file-in-python
-#abspath = os.path.abspath(sys.argv[0])
-abspath = r'C:\Users\Ash\Python Files\Interview_Simulator'
+#https://stackoverflow.com/questions/37912556/converting-a-log-file-into-a-csv-file-using-python
+abspath = os.path.abspath(sys.argv[0])
+#abspath = r'C:\Users\Ash\Python Files\Interview_Simulator'
 for filename in os.listdir(abspath):
     if filename == 'user_response.log':
        f  = open(os.path.join(abspath, 'user_response.log'), "r")
-       #print (f.read()) - Don't need just to check.
-      
-#https://stackoverflow.com/questions/37912556/converting-a-log-file-into-a-csv-file-using-python
-#Opens log file as a list
+
+      #Opens log file as a list
 with open('user_response.log') as file:
     interview = file.read().splitlines()
 
-#Whole interview session is everything before user requests to end the session
+#If user exited session then need to execute so that can capture before user requested to end the session.
 def whole_interview(list):    
     for i in range(len(list)):
         if re.search('end session', list[i]):
@@ -58,15 +54,16 @@ def q_search(list):
         questions.append(q_only)
     return questions
 
+#Isolates the answer's from the user.
+#Adpated from: #https://stackoverflow.com/questions/41125909/python-find-elements-in-one-list-that-are-not-in-the-other
 def a_search(interview):
-#seperate out all of IRIS's statements - Might get rid of category altogether.
+#seperate out all of IRIS's statements
     all_iris = [i for i in interview  if re.search(r'IRIS:', i) ]
-    #Use list comprehension to isolate the user's answers
-    #https://stackoverflow.com/questions/41125909/python-find-elements-in-one-list-that-are-not-in-the-other
+    #Use list comprehension to isolate the user's answers   
     answers_list = [item for item in interview if item not in all_iris]
     #Gets rid of empty rows
     answers_list = list(filter(None,answers_list))
-    #Removes * character - Final list of answers:
+    #Removes * character IRIS denoted if they went overtime.
     answers = [ x for x in answers_list if "*" not in x ]
     
     return answers
@@ -79,7 +76,7 @@ answers = a_search(interview_2)
 questions = q_search(interview)
 
 #Create a dataframe of Interview session with just the questions and answers
-#Will remove any questions where the user had an empty response.
+#Df removes any questions where the user had an empty response.
 interview_df = pd.DataFrame(list(zip(questions, answers)), columns = ['questions', 'answers'])
 #Third column is the score, defaults to 7.
 interview_df['score'] = 7
@@ -94,19 +91,20 @@ def overtime_q(list):
     overtime_qs = []
     for i in range(len(interview)):
         if re.search(asterisk, interview[i]):
+            #Question is 2 rows away, using index to locate question in list
             q_index = i - 2
             overtime_q = interview[q_index]    
             overtime_qs.append(overtime_q)
         
     return overtime_qs
 
-#Cleaning up overtime questoin list by removing asterisk and blank lines of list.
+#Cleaning up overtime question list by removing asterisk and blank lines of list.
 #https://stackoverflow.com/questions/3416401/removing-elements-from-a-list-containing-specific-characters
 all_overtime = overtime_q(interview)    
 all_overtime = [ x for x in all_overtime if "*" not in x ]
 all_overtime = list(filter(None,all_overtime))
 
-#Remove IRIS: Question: from list - Clean so can search dictionary
+#Remove IRIS: Question: so only getting the question text.
 clean_overtime_q =[]
 for i in range(0, len(all_overtime)):
     q = all_overtime[i]
@@ -115,34 +113,37 @@ for i in range(0, len(all_overtime)):
     clean_overtime_q.append(q_only)
 
 #https://stackoverflow.com/questions/37976823/how-to-conditionally-update-dataframe-column-in-pandas-based-on-list
-#1. SCORE - if user went over time for that questions -2 from score
+#1. SCORE - if user went over time for that questions -2 from score column
 interview_df.loc[interview_df.questions.isin(clean_overtime_q), 'score'] = interview_df['score'] - 2
-# add the feedback to the dataframe
-# adds feedback for responses that lost points
-interview_df.loc[interview_df.questions.isin(clean_overtime_q), 'feedback'] = interview_df['feedback'] + ' You took too long to answer.'
-# adds feedback for responses that did not lose points
-interview_df.loc[~interview_df.questions.isin(clean_overtime_q), 'feedback'] = interview_df['feedback'] + ' You answered within 2 minutes.'
+#if user went over time then will add this feedback:
+interview_df.loc[interview_df.questions.isin(clean_overtime_q), 'feedback'] = interview_df['feedback'] 
++ ''' You took too long to answer.'''
+#adds feedback for responses that did not lose points
+interview_df.loc[~interview_df.questions.isin(clean_overtime_q), 'feedback'] = interview_df['feedback'] 
++ ''' You answered within 2 minutes.'''
 
 #Creating a list of all the answers where user responded in less than 3 sentences.
 short_answers = []
 for i in range(0, len(answers)):
+    #Tokenizes each answer and checks the length of sentence tokens.
     sent_tokens = sent_tokenize(answers[i])
     if len(sent_tokens) < 3:
         short_answers.append(answers[i])
 
-#2. SCORE - if user's answer is too short -2 from score again.
+#2.SCORE - if user's answer is too short -2 from score column.
 interview_df.loc[interview_df.answers.isin(short_answers), 'score'] = interview_df['score'] - 2
-# add the feedback to the dataframe
-# adds feedback for responses that lost points
-interview_df.loc[interview_df.answers.isin(short_answers), 'feedback'] = interview_df['feedback'] + ' Your answer is too short. You should take as much time as you need to express your answer with some detail.'
-# adds feedback for responses that did not lose points
-interview_df.loc[~interview_df.answers.isin(short_answers), 'feedback'] = interview_df['feedback'] + ' Your answer was atleast sufficient in length.'
+#adds feedback for responses that lost points
+interview_df.loc[interview_df.answers.isin(short_answers), 'feedback'] = interview_df['feedback'] 
++ ''' Your answer is too short. You should use the time to express your answer with more detail.'''
+#adds feedback for responses that did not lose points
+interview_df.loc[~interview_df.answers.isin(short_answers), 'feedback'] = interview_df['feedback'] 
++ ''' Your answer was sufficient in length.'''
 
-# uses spacy to identify entities, saves entitie text and label to a dictionary
+#uses spacy to identify entities, saves entity text and label to a dictionary
 def get_ents(responses):
     entity ={}
 
-# loop through each answer, and save the entities from the text
+#loop through each answer, and save the entities from the text
     for i in range(0, len(answers)):
         doc = nlp(answers[i])
         for ent in doc.ents:
@@ -155,10 +156,12 @@ def get_ents(responses):
             
     return entity
 
-# evaluates the sentences to see if an Entity is present. If a sentence does not have an entity, save it to list and return list
+#evaluates the sentences to see if an Entity is present. If a sentence does not have an entity, save it to no_entity list.
 def eval_ents(responses):
-    accpetable_entity_dict = {}  # Create a new empty dictionary
-    acceptable_entity_list = []  # Create a new empty list
+    # Create a new empty dictionary
+    accpetable_entity_dict = {} 
+    # Create a new empty list
+    acceptable_entity_list = []  
     no_entity = []
 
     # run the get_ents method to create a list of all entities
@@ -173,23 +176,23 @@ def eval_ents(responses):
     #no_entity = [item for item in answers if item not in acceptable_entity_list]
     #return no_entity
 
-    # loop through the responses to isolate responses with no entities
+    #loop through the responses to isolate responses with no entities
     for i in range(0, len(answers)):
         for j in range(0, len(acceptable_entity_list)):
             
             # search for the entity within the sentence
             if re.search(acceptable_entity_list[j], answers[i]):
-                # if an entity was found, but was added to the no_entity list earlier, try to remove it from the list
+                #if an entity was found, but was added to the no_entity list earlier, try to remove it from the list
                 try:
                     no_entity.remove(answers[i])
                 except ValueError:
                     pass  
-                # if a entity is found in a sentence, no need to search for more entities in that sentence, break to next sentence
+                #if a entity is found in a sentence, no need to search for more entities in that sentence, break to next sentence
                 break
-            # check if a sentence is already in the cumulative list, dont do anything if true (otherwise duplicates sentences)
+            #check if a sentence is already in the cumulative list, dont do anything if true (otherwise duplicates sentences)
             elif answers[i] in no_entity:
                 pass
-            # add the sentence to the cumulative list
+            #add the sentence to the cumulative list
             else:
                 no_entity.append(answers[i])
     return no_entity
@@ -204,35 +207,38 @@ no_ents = eval_ents(answers)
 
 #3. If user's answers used less than 1 named entity
 interview_df.loc[interview_df.answers.isin(no_ents), 'score'] = interview_df['score'] - 3
-# add the feedback to the dataframe
-# adds feedback for responses that lost points
-interview_df.loc[interview_df.answers.isin(no_ents), 'feedback'] = interview_df['feedback'] + ' You did not include identifying information, such as company names. Including these details can give the interviewer a better understanding of your experience.'
-# adds feedback for responses that did not lose points
-interview_df.loc[~interview_df.answers.isin(no_ents), 'feedback'] = interview_df['feedback'] + ' You included identifying information for the organizations you worked for. Great work!'
+#add the feedback to the dataframe
+#adds feedback for responses that lost points
+interview_df.loc[interview_df.answers.isin(no_ents), 'feedback'] = interview_df['feedback'] 
++ ''' You did not include identifying information, such as company names. Including these details can give the 
+interviewer a better understanding of your experience.'''
+#adds feedback for responses that did not lose points
+interview_df.loc[~interview_df.answers.isin(no_ents), 'feedback'] = interview_df['feedback'] 
++ ''' You included identifying information for the organizations you worked for. Great work!'''
 
 # Compile the feedback to be printed and saved to log
 # declare a feedback holder
 compl_feedback = 'FEEDBACK'
 
-# get current date and time for the log file
+#get current date and time for the log file
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 current_date = now.strftime("%B %d, %Y")
 
-# add the date and time to the feedback holder
+#add the date and time to the feedback holder
 compl_feedback += '\nLog file created on: ' + current_date + ' at ' + current_time
 
-# loop through the dataframe and add to the feedback holder
+#loop through the dataframe and add to the feedback holder
 for ind in interview_df.index: 
     compl_feedback += '\n\nQuestion ' + str(ind + 1) + ': ' + interview_df['questions'][ind]
     compl_feedback += '\nAnswer ' + str(ind + 1) + ': ' + interview_df['answers'][ind]
     compl_feedback += "\nScore: " + str(interview_df['score'][ind]) + '/7'
     compl_feedback += "\nDetailed Feedback: " + interview_df['feedback'][ind]
 
-# print the feedback to console
+#print the feedback to console
 print(compl_feedback)
 
-# save the feedback to a log file
+#save the feedback to a text file.
 f = open("feedback.txt", "w")
 f.write(compl_feedback)
 f.close()
