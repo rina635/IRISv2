eac86265361dafe88916e742be1b9c7071d5211c
Rina <rina635@gmail.com>
2021-04-21 14:41:53 -0400

    Cleaned code added invalids to question number

--- iris.py
+++ iris.py
@@ -16,20 +16,19 @@ Resources used for this lab come from the materials provided in the AIT 590 cour
 - Timer examples understood from: https://stackoverflow.com/questions/15528939/python-3-timed-input
 """
 #import and initialize libraries
-import random, re, nltk, os, sys, logging, time
+import random, re , os, sys, logging, time
 import pandas as pd 
-from nltk.tokenize import word_tokenize
 from datetime import datetime
 from threading import Thread
 
-#Creates a log file of all user responses will rewrite the log file every time code executed.
+#Creates a log file of interview session, will rewrite the log file every time code executed.
 #https://stackoverflow.com/questions/38409450/logging-to-python-file-doesnt-overwrite-file-when-using-the-mode-w-argument-t
 #https://www.youtube.com/watch?v=-ARI4Cz-awo&t=616s
 log_filename = 'user_response.log'
 logging.basicConfig(filename= log_filename, level = logging.DEBUG, format = '%(message)s')
 handler = logging.FileHandler(log_filename, 'w+')
 
-# global variables
+
 bot_name = 'IRIS'
 #Name used in chat
 bot_formatted_name = 'IRIS: '
@@ -55,25 +54,26 @@ def greeting():
 def start_conversation():
     # start of program and logic
     # first interaction with user
-    print('{}{}, I am {} the Instructive Response Interview Simulator. Here to help you prepare for your next interview!'.format(bot_formatted_name, greeting(), bot_name))
+    print('{}{}, I am {} the Instructive Response Interview Simulator. Here to help you prepare for your next interview!'
+          .format(bot_formatted_name, greeting(), bot_name))
     
     
     # save the users name as an input
-    name = input(bot_formatted_name + 'Before we begin, what is your name?\n')   # ask for their name and saves inputted value to userName variable
+    name = input(bot_formatted_name + 'Before we begin, what is your name?\n')
     
     # check if name is empty before contnuing
     while name == '':
-        name = input(bot_formatted_name + 'Invalid response. I need to know how to address you. What is your name?\n')   # ask for their name and saves inputted value to userName variable
+        name = input(bot_formatted_name + 'Invalid response. I need to know how to address you. What is your name?\n')
     
-    #Provides instructions to user.
+    
     print(bot_formatted_name + 'Nice to meet you, {}! To end the session, type "END SESSION". For help, type "HELP".'.format(name))  
-    
-    new_user = input(bot_formatted_name + 'Is this your first time interacting with me?\nType "Yes" or "No"\n')   # ask if first time     
+    #Asks if its the user's first time.
+    new_user = input(bot_formatted_name + 'Is this your first time interacting with me?\nType "Yes" or "No"\n')     
 
-    
+    #if neither yes/no is told to IRIS
     while not (new_user.lower() == 'yes' or new_user.lower() == 'no'):
-        new_user = input(bot_formatted_name + 'Invalid response. I need to know if this is your first time using IRIS?\n')   # ask for their name and saves inputted value to userName variable
-  
+        new_user = input(bot_formatted_name + 'Invalid response. I need to know if this is your first time using IRIS?\n') 
+    #if it is the user's first time, executes the instructions.    
     if new_user.lower() == 'yes':
         get_help()
     elif new_user.lower() == 'no':
@@ -87,18 +87,14 @@ def start_conversation():
 
 # calls the instructions for using the program 
 def get_help():
-    print('\n' + bot_formatted_name + '''Welcome my job here is to help you practice for your interview by providing unlimited number of questions.
-     Each question is graded on a scale from 1-10 and you will have up to 2 minutes to respond with your answers.
-     After 2 minutes has been reached there will be a '*' denoted on the log, but you will still be able to respond.
-     Exceeding 2 minutes will result in a 2 point infraction.\n''')
+    print('\n' + bot_formatted_name + '''Welcome, my job here is to help you practice your interview skills. The number of
+          questions I will ask is based on your selection from 5-10. The goal is to ensure you're able to answer these
+          questions the best way you can. Make sure you discuss specific experiences and how you specifically played
+          a role. Each answer should be given within 2 minutes, although I will allow you to answer if you've exceeded 
+          that time, it will negatively affect your grade. 
+          
+          Any blank responses will not be graded.\n''')
     time.sleep(5)
-
-
-# clean the input submitted
-def process_input(user_input):
-    
-    return user_input
-
 
 # loads the questions from the CSV
 def load_questions(file):
@@ -117,7 +113,7 @@ def choose_question(dataframe):
 
     return ques, cat
 
-#IRIS will log when the user has exceeded 2 minutes without a response with asterisk.
+#IRIS will log when the user has exceeded 2 mins without a response with asterisk.
 def idle_check():
     time.sleep(120)
     if answer != None:
@@ -149,15 +145,23 @@ questions_df = load_questions(dname)
 
 # start the conversation
 userName = start_conversation()
-n = int(input('{}How many questions would you like for today? \nPlease select a number between 5 and 10.\n'.format(bot_formatted_name)))
+n = int(input('''{}How many questions would you like for today? \nPlease select a number between 5 and 10.\n'''
+              .format(bot_formatted_name)))
+
 # Reformat the user's name for the chat.
 formattedName = userName + ": "
 
 # Interview session will continue until the number of questions has been reached.
-while question_counter < n: 
+if n < 5:
+    print('{}Invalid number, please select a number between 5 and 10.'.format(bot_formatted_name))
+elif n > 10:
+    print('{}Invalid number, please select a number between 5 and 10.'.format(bot_formatted_name))
+
+while n > 5 and n <= 10 and question_counter < n: 
     
     
     question, category = choose_question(questions_df)
+    #Keeps track of question number
     question_counter = question_counter + 1
     
     print('{}The category is: {}'.format(bot_formatted_name, category))
@@ -169,8 +173,7 @@ while question_counter < n:
     logging.debug('{}The category is: {}'.format(bot_formatted_name, category))
     logging.debug('{}Question {}:{}\n'.format(bot_formatted_name, question_counter, question))
 
-    #records user's responses and processes it.
-    #response = process_input(input(formattedName))
+    #takes user's input
     response = input(formattedName)
     #logging user's response into log file without their name.
     log_response = logging.debug('{}'.format(response))
@@ -187,7 +190,8 @@ while question_counter < n:
 
     # exit the while loop if the user typed end session or END SESSION 
     elif response == 'end session' or response == 'END SESSION':
-        running = False   # changes the running global variable to false to end the program
-        print(bot_formatted_name + "Thanks for an amazing conversation!")
+        break
 
-print(bot_formatted_name + "It was really great learning more about you, the maximum number of questions has been reached.")
+
+#Once question limit is reached/user ends session , will output this:
+print(bot_formatted_name + "It was really great learning more about you, this is the end of our session.")
